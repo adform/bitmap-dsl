e322eafcef0b982177e5f212f0099e1b6332ea01
przemyslaw.piotrowski <przemyslaw.piotrowski@adform.com>
2016-11-18 14:48:28 +0200

    Rewrite to use common test template

--- src/test/scala/com/adform/bitmaps/BitmapExecutorTest.scala
+++ src/test/scala/com/adform/bitmaps/BitmapExecutorTest.scala
@@ -1,100 +1,102 @@
 package com.adform.bitmaps
 
-import com.adform.bitmaps.util.TestDB
-import org.roaringbitmap.RoaringBitmap
+import com.adform.bitmaps.Implicits.RoaringBitmapImpl
+import com.adform.bitmaps.util.BitmapTest
 import org.roaringbitmap.RoaringBitmap.bitmapOf
-import org.scalatest.{MustMatchers, WordSpecLike}
+import org.scalatest.WordSpecLike
 
 
-class BitmapExecutorTest extends MustMatchers with WordSpecLike {
-
-  import com.adform.bitmaps.Implicits.RoaringBitmapImpl
+class BitmapExecutorTest extends BitmapTest with WordSpecLike {
 
   "BitmapExecutor" must {
 
     "calculate some example (a AND (b OR c OR d)) AND e" in {
-      val expression = And(And(Get("a"), Or(Get("b"), Get("c"), Get("d"))), Get("e"))
-      val db: BitmapDB[RoaringBitmap] = TestDB(Map(
-        "a" -> bitmapOf(1, 3, 6, 8, 11),
-        "b" -> bitmapOf(1, 2, 6, 11),
-        "c" -> bitmapOf(3, 6, 777),
-        "d" -> bitmapOf(3, 6, 999),
-        "e" -> bitmapOf(1, 3, 5, 7, 9)
-      ))
-
-      val result = new BitmapExecutor(db).execute(expression)
-
-      result mustBe bitmapOf(1, 3)
+      check(
+        expr =
+          And(
+            And(
+              Get("a"),
+              Or(
+                Get("b"),
+                Get("c"),
+                Get("d"))),
+            Get("e")),
+        db = Map(
+          "a" -> bitmapOf(1, 3, 6, 8, 11),
+          "b" -> bitmapOf(1, 2, 6, 11),
+          "c" -> bitmapOf(3, 6, 777),
+          "d" -> bitmapOf(3, 6, 999),
+          "e" -> bitmapOf(1, 3, 5, 7, 9)),
+        expected = bitmapOf(1, 3))
     }
 
-    "calculate full example " +
-      "(inv1 OR inv2 OR (inv4 AND packg4 AND deal4) OR inv7) AND " +
-      "¬(cat100 OR cat200 OR cat300) AND " +
-      "((mobile AND ios) AND ( (pl OR dennmark) AND ¬(zip1 OR zip2) ))" in {
-
-      val inventories = Or(Get("inv1"), Get("inv2"), And(Get("inv4"), Get("packg4"), Get("deal4")), Get("inv7"))
-      val categories = Not(Or(Get("cat100"), Get("cat200"), Get("cat300")))
-      val audience = {
-        val device = And(Get("mobile"), Get("ios"))
-        val location = And(Or(Get("pl"), Get("denmark")), Not(Or(Get("zip1"), Get("zip2"))))
-        And(device, location)
-      }
-      val expression = And(inventories, categories, audience)
-      val db: BitmapDB[RoaringBitmap] = TestDB(Map(
-        "inv1" -> bitmapOf(1, 101, 201, 301, 401),
-        "inv2" -> bitmapOf(2, 102, 202, 302, 402),
-        "inv4" -> bitmapOf(4, 104, 204, 304, 404),
-        "inv7" -> bitmapOf(7, 107, 207, 307, 407),
-        "packg4" -> bitmapOf(4, 104, 204, 304),
-        "deal4" -> bitmapOf(104, 204, 304, 404),
-        "cat100" -> bitmapOf(101, 102, 104, 107),
-        "cat200" -> bitmapOf(201, 202, 204, 207),
-        "cat300" -> bitmapOf(301, 302, 304, 307),
-        "mobile" -> bitmapOf(1, 2, 7, 401, 402, 407),
-        "ios" -> bitmapOf(2, 3, 7, 401),
-        "pl" -> bitmapOf(2, 3),
-        "denmark" -> bitmapOf(7),
-        "zip1" -> bitmapOf(3),
-        "zip2" -> bitmapOf(1)
-      ))
-
-      val result = new BitmapExecutor(db).execute(expression)
-      val expected = bitmapOf(2, 7)
-
-      result mustBe expected
+    "calculate complicated expression example" in {
+      check(
+        expr = And(
+          Or(
+            Get("a"),
+            Get("b"),
+            And(
+              Get("c"),
+              Get("p4"),
+              Get("d4")),
+            Get("d")),
+          Not(
+            Or(Get("c1"), Get("c2"), Get("c3"))),
+          And(
+            And(
+              Get("m"),
+              Get("i")),
+            And(
+              Or(Get("p"), Get("dk")),
+              Not(
+                Or(Get("z1"), Get("z2")))))
+        ),
+        db = Map(
+          "a" -> bitmapOf(1, 101, 201, 301, 401),
+          "b" -> bitmapOf(2, 102, 202, 302, 402),
+          "c" -> bitmapOf(4, 104, 204, 304, 404),
+          "d" -> bitmapOf(7, 107, 207, 307, 407),
+          "p4" -> bitmapOf(4, 104, 204, 304),
+          "d4" -> bitmapOf(104, 204, 304, 404),
+          "c1" -> bitmapOf(101, 102, 104, 107),
+          "c2" -> bitmapOf(201, 202, 204, 207),
+          "c3" -> bitmapOf(301, 302, 304, 307),
+          "m" -> bitmapOf(1, 2, 7, 401, 402, 407),
+          "i" -> bitmapOf(2, 3, 7, 401),
+          "p" -> bitmapOf(2, 3),
+          "dk" -> bitmapOf(7),
+          "z1" -> bitmapOf(3),
+          "z2" -> bitmapOf(1)
+        ),
+        expected = bitmapOf(2, 7))
     }
 
     "not ignore the audience with only negation" in {
-      val audience1 = Get("device|2")
-      val audience2 =
-        And(
-          Not(
-            Or(
-              Get("country_id|276"),
-              Get("country_id|208"),
-              Get("country_id|826")
-            )))
-      val expression =
-        And(
+      check(
+        expr = And(
           Or(
-            Get("inv|1"),
-            Get("inv|3")),
+            Get("a"),
+            Get("c")),
           Or(
-            audience1,
-            audience2
-          ))
-      val db: BitmapDB[RoaringBitmap] = TestDB(Map(
-        "inv|1" -> bitmapOf(1, 101, 201, 301, 401),
-        "inv|3" -> bitmapOf(2, 102, 202, 302, 402),
-        "device|2" -> bitmapOf(2, 102, 201, 202, 302, 402),
-        "country_id|276" -> bitmapOf(2, 302),
-        "country_id|208" -> bitmapOf(102, 402),
-        "country_id|826" -> bitmapOf(301, 401),
-        "FULL" -> bitmapOf(1, 2, 101, 102, 201, 202, 301, 302, 401, 402)))
-
-      val result = new BitmapExecutor(db).execute(expression)
-
-      result mustBe bitmapOf(2, 102, 201, 202, 302, 402, 1, 101)
+            Get("d2"),
+            And(
+              Not(
+                Or(
+                  Get("c276"),
+                  Get("c208"),
+                  Get("c826")
+                )))
+          )),
+        db = Map(
+          "a" -> bitmapOf(1, 101, 201, 301, 401),
+          "c" -> bitmapOf(2, 102, 202, 302, 402),
+          "d2" -> bitmapOf(2, 102, 201, 202, 302, 402),
+          "c276" -> bitmapOf(2, 302),
+          "c208" -> bitmapOf(102, 402),
+          "c826" -> bitmapOf(301, 401),
+          "FULL" -> bitmapOf(1, 2, 101, 102, 201, 202, 301, 302, 401, 402)),
+        expected = bitmapOf(2, 102, 201, 202, 302, 402, 1, 101))
     }
   }
 }
